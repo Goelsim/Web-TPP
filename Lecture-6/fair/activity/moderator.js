5035b8afd9c5f80b319de85dc2c7d27ab0cd9cf5
Goelsim <simran.goel1998@gmail.com>
2020-04-27 21:43:06 +0530

    Updated

--- Lecture-6/fair/activity/moderator.js
+++ Lecture-6/fair/activity/moderator.js
@@ -3,22 +3,28 @@ require("chromedriver");
 let swd = require("selenium-webdriver");
 let bldr = new swd.Builder();
 let driver = bldr.forBrowser("chrome").build();
-let cfile = process.argv[2];
+let cFile = process.argv[2];
 let uToAdd = process.argv[3];
 (async function () {
     try {
+        await driver.manage().setTimeouts({ implicit: 10000, pageLoad: 10000 });
         let data = await fs.promises.readFile(cFile);
         let { url, pwd, user } = JSON.parse(data);
         await driver.get(url);
-        let unInputWillBeFPromise = driver.findElement(swd.By.css('#input-1'));
-        let psInputWillBeFPromise = driver.findElement(swd.By.css('#input-2'));
+        let unInputWillBeFPromise = driver.findElement(swd.By.css("#input-1"));
+        let psInputWillBeFPromise = driver.findElement(swd.By.css("#input-2"));
         let unNpsEl = await Promise.all([unInputWillBeFPromise, psInputWillBeFPromise]);
         let uNameWillBeSendPromise = unNpsEl[0].sendKeys(user);
         let pWillBeSendPromise = unNpsEl[1].sendKeys(pwd);
         await Promise.all([uNameWillBeSendPromise, pWillBeSendPromise]);
-        let loginBtn = await driver.findElement(swd.By.css("button[data-analytics=LoginPassword"));
+        let loginBtn = await driver.findElement(swd.By.css("button[data-analytics=LoginPassword]"));
         await loginBtn.click();
-        console.log("We have logged in");
+        //console.log("We have logged in");
+        let adminLinkAnchor = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDownAdministration]"));
+        let adminPageUrl = await adminLinkAnchor.getAttribute("href");
+        await driver.get(adminPageUrl);
+        let manageTabs = await driver.findElement(swd.By.css(".administration header ul li"));
+        await manageTabs[1].click();
     }
     catch(err) {
         console.log(err);
