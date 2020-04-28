3f0ddd66cbb115a956ba0619b23005acd30e8457
Goelsim <simran.goel1998@gmail.com>
2020-04-28 17:04:43 +0530

    Updated

--- Lec-Promise_async_await/loop/loop_serial_promises.js
+++ Lec-Promise_async_await/loop/loop_serial_promises.js
@@ -1,18 +1,6 @@
 let fs = require("fs");
-// readFiles(0)
-// sequentially n number files read
-// function readFiles(i) {
-  //   if (i == files.length) {
-    //     return;
-    //   }
-    //   fs.readFile(files[i], function (err, data) {
-      //     // console.log(err);
-      //     console.log(`Data of file${i + 1} : ${data.byteLength}`);
-      //     readFiles(i + 1);
-      //   })
-      // }
-      function promiseMultiFileReader() {
-        let files = ["../f1.txt", "../f2.txt", "../f3.txt", "../f4.txt", "../f5.txt"];
+function promiseMultiFileReader() {
+  let files = ["../f1.txt", "../f2.txt", "../f3.txt", "../f4.txt", "../f5.txt"];
 
   let file1WillBeReadPromise = fs.promises.readFile(files[0]);
   for (let i = 1; i < files.length; i++) {
@@ -25,7 +13,7 @@ let fs = require("fs");
   }
   return file1WillBeReadPromise;
 }
-promiseMultiFileReader().then(function(data){
+promiseMultiFileReader().then(function (data) {
   console.log("file 5 will be printed");
   console.log(data);
 })
